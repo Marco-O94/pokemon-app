e97344028c09b9560518cee17345aea209b9415f
marco.o94 <marcooliveri94@gmail.com>
2024-03-12 21:15:06 +0100

    aggiunti rafce

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import React from "react";
+import Card from "./components/Card";
 function App() {
-  return (
-    <>
-      <div>funziona</div>
-    </>
-  );
+  return <Card />;
 }
 
 export default App;
